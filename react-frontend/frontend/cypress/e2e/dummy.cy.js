5286bb8d51d7c11b83da8185012053ae0c441be4
goncalolima71 <goncalobsflima44@gmail.com>
2025-10-30 00:56:03 +0000

    fix

--- react-frontend/frontend/cypress/e2e/dummy.cy.js
+++ react-frontend/frontend/cypress/e2e/dummy.cy.js
@@ -2,7 +2,7 @@
 
 describe('Dummy E2E', () => {
   it('abre a página inicial do frontend', () => {
-    cy.visit('http://localhost:3000');
+    cy.visit('http://localhost:5173');
     cy.contains('React').should('exist'); // Ajuste o texto conforme o que aparece na sua home
   });
 });

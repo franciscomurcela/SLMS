5b119c90990279a41b4f18953e00f781aa20d93d
goncalolima71 <goncalobsflima44@gmail.com>
2025-11-10 19:48:01 +0000

    update acr_name

--- terraform/main.tf
+++ terraform/main.tf
@@ -57,7 +57,7 @@ resource "random_string" "logs_suffix" {
   upper   = false
 }
 
-# Storage Account (para Blob Storage) yoyoyo
+# Storage Account (para Blob Storage) yoyoyo mr.white
 resource "azurerm_storage_account" "storage" {
   name                     = "slmsstorage${random_string.storage_suffix.result}"
   resource_group_name      = azurerm_resource_group.rg.name

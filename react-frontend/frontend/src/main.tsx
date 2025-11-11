c94b8dc5980887ce81a8b7ecc8e8bd373bcf3beb
goncalolima71 <goncalobsflima44@gmail.com>
2025-11-11 15:33:52 +0000

    fix

--- react-frontend/frontend/src/main.tsx
+++ react-frontend/frontend/src/main.tsx
@@ -3,15 +3,17 @@ import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import App from "./App.tsx";
-import { KeycloakProvider } from "./context/KeycloakContext";
+// TODO: Fix Keycloak redirect loop before re-enabling KeycloakProvider
+// import { KeycloakProvider } from "./context/KeycloakContext";
 import { FeatureFlagsProvider } from "./context/FeatureFlagsContext";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <KeycloakProvider>
-      <FeatureFlagsProvider>
-        <App />
-      </FeatureFlagsProvider>
-    </KeycloakProvider>
+    {/* Temporarily disabled KeycloakProvider to fix redirect loop */}
+    {/* <KeycloakProvider> */}
+    <FeatureFlagsProvider>
+      <App />
+    </FeatureFlagsProvider>
+    {/* </KeycloakProvider> */}
   </StrictMode>
 );
